e8a0013dba25b13f91d20ea9b5b6e5e400da43c6
addyxx-h4ck1 <briannjosh23@gmail.com>
2023-09-08 14:35:23 +0300

    sneakers hub commit

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const responce = fetch ('products.json')
          const i = document.createElement('i')
          i.setAttribute('class', 'fa fa-bolt')
          buttonA.appendChild(i)
-         console.log(i)
+        
          button.appendChild(buttonA)
 
          main.appendChild(image)
@@ -51,11 +51,98 @@ const responce = fetch ('products.json')
 return productData;
 })
 .then(datajson => {
-    console.log(datajson)
     const productArray = datajson.electronics;
 ////////////////////////////Flashsales////////////////////////////////////
        const product = productArray.map(product =>{
             const flashSales = document.getElementById('electronics')
+    
+            const main = document.createElement('div')
+            main.setAttribute('class', 'main-card')
+    
+            const image = document.createElement('img')
+            image.setAttribute(`src`, `${product.image}`)
+    
+            const title = document.createElement('h3')
+            title.setAttribute('class', 'product-title')
+            title.textContent = (`${product.name}`)
+    
+            const price = document.createElement('h4')
+            price.setAttribute('class', 'price')
+            price.textContent = (`${product.price}`)
+
+            const productID = document.createElement('i')
+            productID.textContent = (`product id = ${product.id}`)
+            productID.setAttribute('class', 'product-id')
+            
+    
+            const button = document.createElement('button')
+            const buttonA = document.createElement('a')
+            buttonA.setAttribute('href', 'mailto:briannjosh@gmail.com')
+            buttonA.textContent = ('Buy Now')
+            button.appendChild(buttonA)
+
+            
+    
+            main.appendChild(image)
+            main.appendChild(title)
+            main.appendChild(price)
+            main.appendChild(productID)
+            main.appendChild(button)
+            flashSales.appendChild(main)
+        
+       })
+///////////////////////////Flashsales////////////////////////////////////
+return datajson;
+})
+.then(datajson => {
+    const productArray = datajson.men;
+////////////////////////////Flashsales////////////////////////////////////
+       const product = productArray.map(product =>{
+            const flashSales = document.getElementById('men')
+    
+            const main = document.createElement('div')
+            main.setAttribute('class', 'main-card')
+    
+            const image = document.createElement('img')
+            image.setAttribute(`src`, `${product.image}`)
+    
+            const title = document.createElement('h3')
+            title.setAttribute('class', 'product-title')
+            title.textContent = (`${product.name}`)
+    
+            const price = document.createElement('h4')
+            price.setAttribute('class', 'price')
+            price.textContent = (`${product.price}`)
+
+            const productID = document.createElement('i')
+            productID.textContent = (`product id = ${product.id}`)
+            productID.setAttribute('class', 'product-id')
+            
+    
+            const button = document.createElement('button')
+            const buttonA = document.createElement('a')
+            buttonA.setAttribute('href', 'mailto:briannjosh@gmail.com')
+            buttonA.textContent = ('Buy Now')
+            button.appendChild(buttonA)
+
+            
+    
+            main.appendChild(image)
+            main.appendChild(title)
+            main.appendChild(price)
+            main.appendChild(productID)
+            main.appendChild(button)
+            flashSales.appendChild(main)
+        
+       })
+///////////////////////////Flashsales////////////////////////////////////
+return datajson;
+})
+.then(datajson => {
+    const productArray = datajson.cleaners;
+////////////////////////////Flashsales////////////////////////////////////
+       const product = productArray.map(product =>{
+            const flashSales = document.getElementById('cleaners')
     
             const main = document.createElement('div')
             main.setAttribute('class', 'main-card')
