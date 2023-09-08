4846325d83950208a0b85c9b0a628d35af45a7c7
addyxx-h4ck1 <briannjosh23@gmail.com>
2023-09-08 11:52:56 +0300

    sneakersHub

--- main.js
+++ main.js
@@ -6,36 +6,47 @@ const responce = fetch ('products.json')
     const productData = datajson
    const productArray = datajson.flashSales;
 ////////////////////////////Flashsales////////////////////////////////////
-   const product = productArray.map(product =>{
-        const flashSales = document.getElementById('flash-sales')
+    const product = productArray.map(product =>{
+         const flashSales = document.getElementById('flash-sales')
 
-        const main = document.createElement('div')
-        main.setAttribute('class', 'main-card')
+         const main = document.createElement('div')
+         main.setAttribute('class', 'flash')
 
-        const image = document.createElement('img')
-        image.setAttribute(`src`, `${product.image}`)
+         const image = document.createElement('img')
+         image.setAttribute(`src`, `${product.image}`)
 
-        const title = document.createElement('h3')
-        title.setAttribute('class', 'product-title')
-        title.textContent = (`${product.name}`)
+         const title = document.createElement('h3')
+         title.setAttribute('class', 'flash-title')
+         title.textContent = (`${product.name}`)
 
-        const price = document.createElement('h4')
-        price.setAttribute('class', 'price')
-        price.textContent = (`${product.price}`)
+         const price = document.createElement('h4')
+         price.setAttribute('class', 'flash-price')
+         price.textContent = (`${product.price}`)
 
-        const button = document.createElement('button')
-        const buttonA = document.createElement('a')
-        buttonA.setAttribute('href', 'mailto:briannjosh@gmail.com')
-        buttonA.textContent = ('Buy Now')
-        button.appendChild(buttonA)
+         const productID = document.createElement('i')
+         productID.textContent = (`product id = ${product.id}`)
+         productID.setAttribute('class', 'product-id')
 
-        main.appendChild(image)
-        main.appendChild(title)
-        main.appendChild(price)
-        main.appendChild(button)
-        flashSales.appendChild(main)
+         const button = document.createElement('button')
+         button.setAttribute("class", "flash-button")
+         const buttonA = document.createElement('a')
+         buttonA.setAttribute('href', 'mailto:briannjosh@gmail.com')
+         buttonA.textContent = (`Buy  `)
+
+         const i = document.createElement('i')
+         i.setAttribute('class', 'fa fa-bolt')
+         buttonA.appendChild(i)
+         console.log(i)
+         button.appendChild(buttonA)
+
+         main.appendChild(image)
+         main.appendChild(title)
+         main.appendChild(price)
+         main.appendChild(productID)
+         main.appendChild(button)
+         flashSales.appendChild(main)
     
-   })
+    })
 ///////////////////////////Flashsales////////////////////////////////////
 return productData;
 })
@@ -59,16 +70,24 @@ return productData;
             const price = document.createElement('h4')
             price.setAttribute('class', 'price')
             price.textContent = (`${product.price}`)
+
+            const productID = document.createElement('i')
+            productID.textContent = (`product id = ${product.id}`)
+            productID.setAttribute('class', 'product-id')
+            
     
             const button = document.createElement('button')
             const buttonA = document.createElement('a')
             buttonA.setAttribute('href', 'mailto:briannjosh@gmail.com')
             buttonA.textContent = ('Buy Now')
             button.appendChild(buttonA)
+
+            
     
             main.appendChild(image)
             main.appendChild(title)
             main.appendChild(price)
+            main.appendChild(productID)
             main.appendChild(button)
             flashSales.appendChild(main)
         
